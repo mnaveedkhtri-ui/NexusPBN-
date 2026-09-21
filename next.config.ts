20f50cebe8cfe7b9c809b93723d4bcb5461f41f6
Naveed <m.naveedkhtri@gmail.com>
2026-09-21 14:50:37 +0500

    fix(images): whitelist pollinations generative ai domain in next config to fix blank hero images

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'image.pollinations.ai',
+      },
+      {
+        protocol: 'https',
+        hostname: 'tse1.mm.bing.net',
+      }
+    ],
+  },
 };
 
 export default nextConfig;
